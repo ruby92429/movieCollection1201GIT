cef3c4f9f23e810a800f63be1d5c5441db02e34c
pin789 <ruby789@gmail.com>
2022-12-08 01:22:27 +0800

    與dbjson串接API

--- movieCollection1201.js
+++ movieCollection1201.js
@@ -1,6 +1,12 @@
-$(window).scroll(function (evt) {
-  if ($(window).scrollTop() > 0) $(".navbar").removeClass("navbar-show");
-  else $(".navbar").addClass("navbar-show");
-});
+// $(window).scroll(function (evt) {
+//   if ($(window).scrollTop() > 0) $(".navbar").removeClass("navbar-show");
+//   else $(".navbar").addClass("navbar-show");
+// });
 
-var s = skrollr.init();
+// var s = skrollr.init();
+
+console.log("test");
+
+axios.get(`http://localhost:3000/users`).then(function (response) {
+  console.log(response);
+});
