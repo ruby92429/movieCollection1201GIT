2efcd91f3d56e89caa395e7db6cc88397284f42b
pin789 <ruby789@gmail.com>
2022-12-09 16:55:07 +0800

    做完搜尋功能(但版面未調整)

--- movieCollection1201.js
+++ movieCollection1201.js
@@ -1,12 +1,101 @@
-// $(window).scroll(function (evt) {
-//   if ($(window).scrollTop() > 0) $(".navbar").removeClass("navbar-show");
-//   else $(".navbar").addClass("navbar-show");
-// });
+console.clear();
 
-// var s = skrollr.init();
+let movieData = []; // 雲端資料的本地紀錄
 
-console.log("test");
+// 關鍵字搜尋
+const searchInput = document.querySelector(".keyword");
+const searchBtn = document.querySelector(".searchBtn");
+searchBtn.addEventListener("click", keywordSearch);
+let keyword = "";
+function keywordSearch() {
+  keyword = searchInput.value.trim().toLowerCase();
+  console.log(keyword);
+  compare();
+}
 
-axios.get(`http://localhost:3000/users`).then(function (response) {
-  console.log(response);
-});
+async function compare() {
+  movieData = await getMovie(); //得到雲端資料的本地紀錄
+  console.log(movieData);
+  let targetMovie = [];
+  targetMovie = movieData.filter(function (item) {
+    let title = item.title.toLowerCase();
+    return title.match(keyword);
+  });
+  renderMovie(targetMovie);
+}
+
+const baseUrl = "http://localhost:3000";
+
+function getMovie() {
+  // let url = `${baseUrl}/movies?_limit=1`;
+  let url = `${baseUrl}/movies`;
+  return axios
+    .get(url)
+    .then(function (res) {
+      console.log(res);
+      return res.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+const defNew = document.querySelector(".defNew");
+function renderMovie(movie) {
+  console.log(movie);
+  let str = "";
+  movie.forEach((item) => {
+    str += `
+    <div class="col-sm-12"></div>
+    <!-- Bootstrap格線系統與RWD的應用 -->
+
+    <div class="new">
+      <div class="mainTitle">搜尋結果</div>
+      <div class="subTitle">詳請點擊 more 進入~</div>
+    </div>
+
+    <div class="col-md-4 col-sm-6">
+      <div class="new_wrap">
+        <div class="new_item pic">
+          <img
+            src="${item.cover}"
+            alt=""
+            width="700"
+            height="400"
+          />
+        </div>
+
+        <div class="new_item text">
+          <h4>
+            <!-- <div class="html5-video-container" data-layer="0"><video tabindex="-1" class="video-stream html5-main-video" controlslist="nodownload" src="blob:https://www.youtube.com/fd04e121-c822-4f07-bf84-4a541385e4bc" style="width: 894px; height: 503px; left: 0px; top: 0px;"></video></div> -->
+            <!-- <video src="clip.mp4" controls></video> -->
+            <iframe
+              width="300"
+              height="200"
+              src="${item.video}"
+              title="${item.title}"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+            <br />
+            <h1>${item.title}</h1>
+            <p>
+            ${item.overview}
+
+              <a href="" class="btn">more</a>
+            </p>
+          </h4>
+        </div>
+      </div>
+    </div>
+
+    <div class="defNew_left_top"></div>
+    <div class="defNew_right_top"></div>
+    <div class="defNew_left_bottom"></div>
+    <div class="defNew_right_bottom"></div>
+  </div>
+    `;
+  });
+  defNew.innerHTML = str;
+}
